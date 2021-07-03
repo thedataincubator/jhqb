8d61f9512965c47fa499350afe0908655dc41bb9
Robert Schroll <rschroll@gmail.com>
2021-07-03 15:01:34 -0700

    Allow sorting of questions

--- client/src/Questions.svelte
+++ client/src/Questions.svelte
@@ -13,7 +13,17 @@
   }
 
   let scrollId = null
+  let sortVal = 'date'
+  let sortDir = +1
   const queryResult = useQuery('questions', fetchQuestions)
+
+  // Mark as reactive so sort is recalculated when variables change
+  $: sortQuestions = (a, b) => {
+    let diff = (sortVal === 'date'
+                ? new Date(a.created) - new Date(b.created)
+                : a.votes.length - b.votes.length)
+    return sortDir * diff
+  }
 
   afterUpdate(() => {
     if (scrollId !== null) {
@@ -27,13 +37,42 @@
   })
 </script>
 
+<div class="options">
+  <span class="sort">
+    Sort by
+    <button
+      on:click={() => {
+        if (sortVal !== 'date') {
+          sortVal = 'date'
+          sortDir = +1
+        } else {
+          sortDir = -sortDir
+        }
+      }}
+      class:sortUp={sortVal === 'date' && sortDir === 1}
+      class:sortDown={sortVal === 'date' && sortDir === -1}
+    >Date</button>
+    <button
+      on:click={() => {
+        if (sortVal !== 'votes') {
+          sortVal = 'votes'
+          sortDir = -1
+        } else {
+          sortDir = -sortDir
+        }
+      }}
+      class:sortUp={sortVal === 'votes' && sortDir === 1}
+      class:sortDown={sortVal === 'votes' && sortDir === -1}
+    >Votes</button>
+  </span>
+</div>
 <div class="questions">
   {#if $queryResult.isLoading}
-    <span>Loading...</span>
+    <span class="message">Loading...</span>
   {:else if $queryResult.error}
-    <span>Error: {$queryResult.error.message}</span>
+    <span class="message">Error: {$queryResult.error.message}</span>
   {:else}
-    {#each $queryResult.data as question}
+    {#each $queryResult.data.sort(sortQuestions) as question}
       <Question {...question} />
     {/each}
   {/if}
@@ -50,10 +89,18 @@
     max-width: 75ex;
   }
 
-  div.questions {
+  div.options {
     border-radius: 0.5em 0.5em 0 0 ;
+    border-bottom: none;
+  }
+  div.options * {
+    font-size: small;
+    color: #666;
+  }
+
+  div.questions {
     height: 70vh;
-    overflow-y: scroll;
+    overflow-y: auto;
   }
 
   div.ask {
@@ -61,11 +108,43 @@
     border-top: none;
   }
 
-  span {
+  span.message {
     display: block;
     width: 100%;
     margin-top: 20vh;
     text-align: center;
     font-size: larger;
+  }
+
+  span.sort {
+    margin-left: 1.3em;
+  }
+
+  button {
+    margin: 0;
+    background-color: #eee;
+    border: none;
+    padding-left: 2ex;
+    padding-right: 2ex;
+    cursor: pointer;
+  }
+
+  button.sortUp, button.sortDown {
+    background-color: #ddd;
+  }
+
+  button::after {
+    display: inline-block;
+    width: 1ex;
+    padding-left: 1ex;
+    content: "\a0";
+  }
+
+  button.sortUp::after {
+    content: "▴";
+  }
+
+  button.sortDown::after {
+    content: "▾";
   }
 </style>
